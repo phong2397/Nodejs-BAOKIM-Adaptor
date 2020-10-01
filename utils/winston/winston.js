380c2a714e3eb765d708d39dedeeb57c968e069a
Tran Gia Bao <bao.tran@tienve.com.vn~>
2020-10-01 17:16:58 +0700

    fix winston.js

--- utils/winston/winston.js
+++ utils/winston/winston.js
@@ -1,31 +1,47 @@
-const winston = require("winston");
+var winston = require("winston");
+const { combine, timestamp, label, printf, prettyPrint } = winston.format;
 
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.json(),
-  defaultMeta: { service: "user-service" },
-  transports: [
-    //
-    // - Write all logs with level `error` and below to `error.log`
-    // - Write all logs with level `info` and below to `combined.log`
-    //
-    new winston.transports.File({
-      filename: "logs/error/error.log",
-      level: "error",
-    }),
-    new winston.transports.File({ filename: "logs/combined/combined.log" }),
-  ],
+const consoleFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
-if (process.env.NODE_ENV !== "production") {
-  logger.add(
-    new winston.transports.Console({
-      format: winston.format.simple(),
-    })
-  );
-}
+var options = {
+  file: {
+    level: "info",
+    filename: "logs/app.log",
+    handleExceptions: true,
+    json: false,
+    maxsize: 5242880, // 5MB
+    maxFiles: 10,
+    colorize: false,
+  },
+  console: {
+    level: "debug",
+    format: consoleFormat,
+    handleExceptions: true,
+    json: false,
+    colorize: true,
+  },
+};
+
+var logger = (_label) => {
+  var _logger = winston.createLogger({
+    format: combine(label({ label: _label }), timestamp(), consoleFormat),
+    transports: [
+      new winston.transports.File(options.file),
+      new winston.transports.Console(options.console),
+    ],
+    exitOnError: false, // do not exit on handled exceptions
+  });
+
+  _logger.stream = {
+    write: function (message, encoding) {
+      // use the 'info' log level so the output will be picked up by both transports (file and console)
+      _logger.info(message.replace(/\r?\n|\r/g, " "));
+    },
+  };
+
+  return _logger;
+};
+
 module.exports = logger;
