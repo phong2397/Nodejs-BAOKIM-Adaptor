bf7c72604baf9c704b1843f2223f88463199bc02
Tran Gia Bao <bao.tran@tienve.com.vn~>
2020-10-01 16:35:56 +0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,105 @@
+var { v4: uuidv4 } = require("uuid");
+var express = require("express");
+var router = express.Router();
+var util = require("../utils/util");
+var fs = require("fs");
+var logger = require("../utils/winston/winston");
+var appRootPath = require("app-root-path");
+var privateKey = fs.readFileSync(`${appRootPath}/keyRSA/private.pem`);
+var publickey = fs.readFileSync(`${appRootPath}/keyRSA/public.pem`);
+var baoKimPublicKey = fs.readFileSync(
+  `${appRootPath}/keyRSA/baokim/public.pem`
+);
+
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.post("/notify", function (req, res, next) {
+  let requestInfo = req.body;
+  //check
+  logger.info("webhook", requestInfo);
+  return res.status(200).json({ err_code: "0", message: "recieved" });
+});
+router.post("/notifyCollection", function (req, res, next) {
+  // console.log(req.body);
+  logger.info("collection", req.body);
+  let requestInfo = {
+    RequestId: req.body.RequestId,
+    RequestTime: req.body.RequestTime,
+    PartnerCode: req.body.PartnerCode,
+    AccNo: req.body.AccNo,
+    ClientIdNo: req.body.ClientIdNo,
+    TransId: req.body.TransId,
+    TransAmount: req.body.TransAmount,
+    TransTime: req.body.TransTime,
+    BefTransDebt: req.body.BefTransDebt,
+    AffTransDebt: req.body.AffTransDebt,
+    AccountType: req.body.AccountType,
+    OrderId: req.body.OrderId,
+    Signature: req.body.Signature,
+  };
+
+  let dataFromRequest = `${requestInfo.RequestId}|${requestInfo.RequestTime}|${requestInfo.PartnerCode}|${requestInfo.AccNo}|${requestInfo.ClientIdNo}|${requestInfo.TransId}|${requestInfo.TransAmount}|${requestInfo.TransTime}|${requestInfo.BefTransDebt}|${requestInfo.AffTransDebt}|${requestInfo.AccountType}|${requestInfo.OrderId}`;
+  if (!requestInfo.Signature)
+    return res.status(200).json({
+      ResponseCode: 120,
+      ResponseMessage: "Signature is incorrect",
+    });
+  let checkSignature = util.baokimVerifySignature(
+    dataFromRequest,
+    requestInfo.Signature,
+    baoKimPublicKey
+  );
+  //Verify Signature
+  // if (!checkSignature)
+  //   return res.status(200).json({
+  //     ResponseCode: 120,
+  //     ResponseMessage: "Signature is incorrect",
+  //   });
+  let responseCode = 200;
+  let responseMessage = "Success";
+  let referenceId = `${PARTNERCODE}${uuidv4()}`;
+  let accNo = requestInfo.AccNo;
+  let affTransDebt = requestInfo.AffTransDebt;
+  let rawData = `${responseCode}|${responseMessage}|${referenceId}|${accNo}|${affTransDebt}`;
+  let signature = util.createRSSAignature(rawData, privateKey);
+  return res.status(200).json({
+    ResponseCode: responseCode,
+    ResponseMessage: responseMessage,
+    ReferenceId: referenceId,
+    AccNo: accNo,
+    AffTransDebt: affTransDebt,
+    Signature: signature,
+  });
+});
+router.post("/notifyBankSwitch", function (req, res, next) {
+  logger.info("bankswitch", req.body);
+  let requestInfo = {
+    RequestId: req.body.RequestId,
+    RequestTime: req.body.RequestTime,
+    PartnerCode: req.body.PartnerCode,
+    AccName: req.body.AccName,
+    AccNo: req.body.AccNo,
+    ExpirDate: req.body.ExpirDate,
+    OrderId: req.body.OrderId,
+    BankSortName: req.body.BankSortName,
+  };
+  let Signature = request.headers.Signature;
+  if (Signature)
+    return res.status(200).json({
+      ResponseCode: 120,
+      ResponseMessage: "Signature is incorrect",
+    });
+  let responseInfo = {
+    ResponseCode: 200,
+    ResponseMessage: "Success",
+    AccNo: requestInfo.AccNo,
+  };
+  let signatureRes = util.createRSSAignature(
+    JSON.stringify(responseInfo),
+    privateKey
+  );
+  responseInfo.Signature = signatureRes;
+  return res.status(200).json(responseInfo);
+});
+module.exports = router;
---
+++ utils/util.js
@@ -0,0 +1,85 @@
+const crypto = require("crypto");
+var jwt = require("jsonwebtoken");
+var moment = require("moment");
+
+// TODO: take const data to config
+const API_KEY = "a18ff78e7a9e44f38de372e093d87ca1";
+const API_SECRET = "9623ac03057e433f95d86cf4f3bef5cc";
+const TOKEN_EXPIRE = 60 * 1000; //token expire time in seconds
+const ENCODE_ALG = "HS256";
+
+var setToken = () => {
+  var tokenId = crypto.randomBytes(32).toString("base64");
+  var issuedAt = moment().unix() - 10000;
+  var notBefore = issuedAt;
+  var expire = notBefore + TOKEN_EXPIRE;
+
+  var payload = {
+    iat: issuedAt, // Issued at: time when the token was generated
+    jti: tokenId, // Json Token Id: an unique identifier for the token
+    iss: API_KEY, // Issuer
+    nbf: notBefore, // Not before
+    exp: expire, // Expire
+  };
+
+  var token = jwt.sign(payload, API_SECRET, { algorithm: ENCODE_ALG });
+
+  return token;
+};
+
+var getToken = (token) => {
+  try {
+    token = jwt.verify(token, API_SECRET, { algorithm: ENCODE_ALG });
+  } catch (err) {
+    log.error(err);
+  }
+  return token;
+};
+
+var geerongEncrypt = (text, key) => {
+  var prefix = "Encrypt";
+  var cipher = crypto.createCipheriv("aes-128-ecb", key, "");
+  var encrypted = cipher.update(text, "utf8", "base64");
+  encrypted += cipher.final("base64");
+  encrypted = `${prefix}${encrypted}`;
+  return encrypted;
+};
+
+var geerongDecrypt = (encryptedData, key) => {
+  try {
+    var _encryptedData = encryptedData.substring(7); // remove prefix 'Encrypt'
+    var decipher = crypto.createDecipheriv("aes-128-ecb", key, "");
+    var decrypted = decipher.update(_encryptedData, "base64", "utf8");
+    decrypted += decipher.final("utf8");
+  } catch (e) {
+    log.error("Data cannot decrypt");
+    // throw { Message: 'Err: Data cannot decrypt' };
+    throw `Error: cannot decrypt with "${encryptedData}"`;
+  }
+
+  return decrypted;
+};
+
+var createRSASignature = (requestText, privateKey) => {
+  var sign = crypto.createSign("RSA-SHA1");
+  sign.update(requestText);
+  sign.end();
+  var signature = sign.sign(privateKey, "base64");
+  return signature;
+};
+
+var baokimVerifySignature = (responseText, signature, publicKey) => {
+  const verify = crypto.createVerify("RSA-SHA1");
+  verify.update(responseText);
+  verify.end();
+  return verify.verify(publicKey, Buffer.from(signature, "base64"));
+};
+
+module.exports = {
+  setToken: setToken,
+  getToken: getToken,
+  geerongEncrypt: geerongEncrypt,
+  geerongDecrypt: geerongDecrypt,
+  baokimVerifySignature: baokimVerifySignature,
+  createRSSAignature: createRSASignature,
+};
