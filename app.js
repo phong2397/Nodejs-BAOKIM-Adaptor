a86cec49bf441fa0145adff84990d2205baae314
Tran Gia Bao <bao.tran@tienve.com.vn~>
2020-10-06 14:38:57 +0700

    edit file logs

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.static(path.join(__dirname, "logs")));
+app.use("/logs", express.static(path.join(__dirname, "logs")));
 app.use("/", indexRouter);
 
 // catch 404 and forward to error handler
