a06cd27ad61fdc37a9b1681976bace9050e18ac5
Tran Gia Bao <bao.tran@tienve.com.vn~>
2020-10-14 15:26:49 +0700

    Public logs

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "logs")));
 app.use("/", indexRouter);
 app.use("/logs", express.static(path.join(__dirname, "logs")));
 app.use("/virtualaccount", virtualAccountRoute);
